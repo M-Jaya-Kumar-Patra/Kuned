4d425e3d8b01e6bac7904f7c5a8486b02ca0d291
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-03-19 04:55:56 +0530

    fix: cashfree.d.ts

--- src/types/cashfree.d.ts
+++ src/types/cashfree.d.ts
@@ -2,6 +2,24 @@ export {};
 
 declare global {
   interface Window {
-    Cashfree: any;
+    Cashfree: {
+      new (options: { mode: "sandbox" | "production" }): {
+        checkout: (options: {
+          paymentSessionId: string;
+          redirectTarget?: "_self" | "_blank" | "_modal";
+        }) => void;
+      };
+    };
   }
+}
+
+declare module "@cashfreepayments/cashfree-js" {
+  export function load(options: {
+    mode: "sandbox" | "production";
+  }): Promise<{
+    checkout: (options: {
+      paymentSessionId: string;
+      redirectTarget?: "_self" | "_blank" | "_modal";
+    }) => Promise<void>;
+  }>;
 }
