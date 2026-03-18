1c1281129e0e9f403972312521ba661462112399
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-03-19 05:03:53 +0530

    fix: tsconfig.json

--- src/types/cashfree.d.ts
+++ src/types/cashfree.d.ts
@@ -1,18 +1,3 @@
-export {};
-
-declare global {
-  interface Window {
-    Cashfree: {
-      new (options: { mode: "sandbox" | "production" }): {
-        checkout: (options: {
-          paymentSessionId: string;
-          redirectTarget?: "_self" | "_blank" | "_modal";
-        }) => void;
-      };
-    };
-  }
-}
-
 declare module "@cashfreepayments/cashfree-js" {
   export function load(options: {
     mode: "sandbox" | "production";
