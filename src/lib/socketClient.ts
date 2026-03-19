2d1092a1e7311983c33e42ce459f28d1e293eb4a
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-03-19 13:00:49 +0530

    socker server uri updated

--- src/lib/socketClient.ts
+++ src/lib/socketClient.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL!;
+
+export const socket = io(URL, {
   transports: ["websocket"],
-});
+}); 
