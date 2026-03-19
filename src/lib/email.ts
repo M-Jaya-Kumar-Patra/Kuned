295507ce6778367668a4f3561f32009f0c8d9483
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-03-19 20:56:32 +0530

    replaced resend by nodemailer

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,6 +1,12 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // app password (NOT your real password)
+  },
+});
 
 export async function sendEmail({
   to,
@@ -12,20 +18,56 @@ export async function sendEmail({
   html: string;
 }) {
   try {
-
-    const data = await resend.emails.send({
-      from: process.env.EMAIL_FROM as string,
-      to: [to], // safer to send as array
+    const info = await transporter.sendMail({
+      from: `"Kuned Support" <${process.env.EMAIL_USER}>`,
+      to,
       subject,
       html,
     });
 
-    return data;
+    return info;
 
   } catch (error) {
-
     console.error("Email send error:", error);
     throw new Error("Email failed");
-
   }
 }
+
+
+
+
+
+
+
+
+// import { Resend } from "resend";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function sendEmail({
+//   to,
+//   subject,
+//   html,
+// }: {
+//   to: string;
+//   subject: string;
+//   html: string;
+// }) {
+//   try {
+
+//     const data = await resend.emails.send({
+//       from: process.env.EMAIL_FROM as string,
+//       to: [to], // safer to send as array
+//       subject,
+//       html,
+//     });
+
+//     return data;
+
+//   } catch (error) {
+
+//     console.error("Email send error:", error);
+//     throw new Error("Email failed");
+
+//   }
+// }
